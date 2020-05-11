79001a003c038f628ee7a1bd2d6e5c54121039f2
Kevin Neyer <kevinneyer@gmail.com>
2020-05-11 03:38:59 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,17 +1,5 @@
-def answer
-  rand(1..6)
-end 
-
-def question 
-  puts "Guess a number between 1 and 6"
-end 
-
-def get_input
-  gets.chomp
-end 
-
 def run_guessing_game
-  
+  number = rand(1..6)
   if get_input == answer
     return "You guessed the correct number"
   elsif get_input == "exit"
